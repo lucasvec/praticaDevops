c39b63946f268bc29f18b9367f1cd9b3e306dead
lucasvec <dosgamesxxx@gmail.com>
2024-02-23 18:10:46 -0300

    Aperfeiçoando comentários.

--- app/src/main/java/com/example/ex5/MainActivity.java
+++ app/src/main/java/com/example/ex5/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Atributos dos elementos que vamos manipular
     TextView inputGasolina;
     TextView inputAlcool;
     TextView textResposta;
@@ -19,19 +20,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Buscando os elementos pelo seu ID e definindo como o valor dos atributos
         inputAlcool = findViewById(R.id.inputAlcool);
         inputGasolina = findViewById(R.id.inputGasolina);
         textResposta = findViewById(R.id.textResposta);
     }
 
-    //Método para calcular preço
+    //Método para calcular qual preço está mais viável
     public void calcularPreco(View view){
 
+        //Criando variáveis com os respectivos tipos primitivos, para que possamos fazer a manipulação
         double precoGasolina = Double.parseDouble(inputGasolina.getText().toString());
         double precoAlcool = Double.parseDouble(inputAlcool.getText().toString());
+
+        //Fórmula para ter o valor de comparação com o índice
         double calculo = precoAlcool / precoGasolina;
+        //Índice
         double indice = 0.7;
 
+        //Se o valor da divisão for maior igual ao indice, gasolina compensa mais, senão o álcool
         if(calculo >= indice){
             textResposta.setText("Gasolina compensa mais.");
         }else{
